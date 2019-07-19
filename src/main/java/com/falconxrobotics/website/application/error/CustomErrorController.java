7b7baf3d55128694643ff48fade09023cb62e5ad
RayBipse <raybipse@gmail.com>
2019-07-18 22:29:39 -0700

    Changed Error Controller

--- src/main/java/com/falconxrobotics/website/application/error/CustomErrorController.java
+++ src/main/java/com/falconxrobotics/website/application/error/CustomErrorController.java
@@ -24,7 +24,6 @@ public class CustomErrorController implements ErrorController {
 
         if (status != null) {
             int code = Integer.parseInt(status.toString());
-
         }
 
         return "error";
