c03fe6ae1e5a3b7c390f1f3aa7193237f460b5d3
FalconX Robotics <FalconX@CPHS-FXR-01.ad.mdusd.org>
2019-07-23 17:00:07 -0700

    Added more css

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+(()=>{
+    const menuButton = document.getElementById("menu-button");
+    const menu = document.getElementById('menu');
+
+    menuButton.addEventListener('click', (_) => {
+        menuButton.classList.toggle("change");
+        menu.classList.toggle('on');
+        if (!menu.classList.contains('on')) {
+            setTimeout(() => {
+                menu.classList.add('off')
+            }, 200);
+        } else {
+            menu.classList.remove('off')
+        }
+    });
+})();
