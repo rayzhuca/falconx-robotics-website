2638d5eecbd89e5f599d555012cb32c21233b607
RayBipse <raybipse@gmail.com>
2019-07-25 22:07:16 -0700

    Modified index.html

---
+++ src/main/resources/public/js/index.js
@@ -0,0 +1,9 @@
+// fetch('/css/index.css').then(e => e.text()).then(css => {
+//     console.log(css);
+//     const cssLink = document.createElement("link");
+//     cssLink.type = "text/css";
+//     cssLink.rel = "stylesheet";
+//     cssLink.textContent = css;
+//     console.log(document.getElementsByTagName('iframe')[0]);
+//     document.getElementsByTagName('iframe')[0].contentWindow.document.head.appendChild(cssLink);
+// });
