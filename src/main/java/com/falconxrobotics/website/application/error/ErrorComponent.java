bcda9590cd499aa04edcd30adf69f3537d39a6ea
RayBipse <raybipse@gmail.com>
2019-08-05 15:00:52 -0700

    Made Error Component functional.

--- src/main/java/com/falconxrobotics/website/application/error/ErrorComponent.java
+++ src/main/java/com/falconxrobotics/website/application/error/ErrorComponent.java
@@ -12,21 +12,61 @@ public class ErrorComponent {
 
     public HashMap<String, String> getAttributes(HttpServletRequest request) {
 
-        final Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        final Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
-        final Object exception = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+        Object exception = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
 
         return getAttributes(status, message, exception);
-
     }
 
     public HashMap<String, String> getAttributes(Object status, Object message, Object exception) {
         HashMap<String, String> attributes = new HashMap<>();
 
-        attributes.put("statuscode", status != null ? status.toString() : null);
+        attributes.put("statuscode", status != null ? status.toString() : "520");
         attributes.put("message", message != null ? message.toString() : null);
-        attributes.put("exception", exception != null ? exception.toString() : null);
+        attributes.put("exception", exception != null ? exception.toString() : "");
+
+        if (attributes.get("messsage") == null) {
+            try {
+                String customMsg = ErrorCode.getMessageFromCode(Integer.parseInt(attributes.get("statuscode")));
+                attributes.put("message", customMsg);
+            } catch (NumberFormatException nfe) {
+                attributes.put("message", "");
+                nfe.printStackTrace();
+            }
+        }
 
         return attributes;
+    }
+
+    private enum ErrorCode {
+        FOUR_ZERO_FOUR(404, "Page not found"), FIVE_TWENTY(520, "Unknown error");
+
+        private int code;
+        private String message;
+
+        private ErrorCode(int code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public static String getMessageFromCode(int code) {
+            for (ErrorCode err : ErrorCode.values()) {
+                if (code == err.getCode()) {
+                    return err.getMessage();
+                }
+            }
+
+            return null;
+        }
+
     }
 }
