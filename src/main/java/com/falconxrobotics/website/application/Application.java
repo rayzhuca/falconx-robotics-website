793ac3fcfdaff9c270d61bdf7bd6ac56d8309c73
Ray Z <raybipse@gmail.com>
2019-12-24 16:58:55 -0800

    Added for testing comments.

--- src/main/java/com/falconxrobotics/website/application/Application.java
+++ src/main/java/com/falconxrobotics/website/application/Application.java
@@ -1,6 +1,7 @@
 package com.falconxrobotics.website.application;
 
 import java.io.IOException;
+// import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) throws IOException {
+		// For testing:
+		// SpringApplication app = new SpringApplication(Application.class);
+		// app.setDefaultProperties(Collections
+        //   .singletonMap("server.port", "80"));
+		// app.run(args);
+		
+		// For release:
 		SpringApplication.run(Application.class, args);
 	}
 
