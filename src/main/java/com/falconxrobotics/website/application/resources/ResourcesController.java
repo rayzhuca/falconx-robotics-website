b393f9fc8fe98d990b13ca021a3fd2f066d5d4ca
Ray Z <raybipse@gmail.com>
2020-01-02 16:05:20 -0800

    Made use of custom component.

--- src/main/java/com/falconxrobotics/website/application/resources/ResourcesController.java
+++ src/main/java/com/falconxrobotics/website/application/resources/ResourcesController.java
@@ -2,35 +2,38 @@ package com.falconxrobotics.website.application.resources;
 
 import java.io.IOException;
 
-import com.falconxrobotics.website.application.googlesheets.SheetComponent;
+import com.falconxrobotics.website.application.error.ErrorComponent;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ResourcesController {
 
-    private SheetComponent sheetComponent;
+    private ResourcesComponent resourcesComponent;
+    private ErrorComponent errorComponent;
 
     @Autowired
-    public ResourcesController(SheetComponent sheetComponent) {
-        this.sheetComponent = sheetComponent;
+    public ResourcesController(ResourcesComponent resourcesComponent, ErrorComponent errorComponent) {
+        this.resourcesComponent = resourcesComponent;
+        this.errorComponent = errorComponent;
     }
 
-    @GetMapping(path = {"/resources/curriculum/programming"})
-    public String programming(Model model) {
+    @GetMapping(path = {"/resources/curriculum"})
+    public String programming(Model model, @RequestParam(name = "name") String name) {
         try {
-            model.addAllAttributes(sheetComponent.getAttributes("home"));
-            return "resources/curriculum/programming";
+            model.addAllAttributes(resourcesComponent.getResourcesAttributes(name));
+            return "resources/curriculum/curriculum";
         } catch (IOException ioe) {
-            ioe.printStackTrace();
-            // TODO: Let error controllers handle
+            model.addAllAttributes(errorComponent.getAttributes("404", null, ioe.getMessage()));
+            return "error";
         } catch (RuntimeException re) {
             re.printStackTrace();
-            // TODO: Let error controllers handle
+            model.addAllAttributes(errorComponent.getAttributes("404", null, re.getMessage()));
+            return "error";
         }
-        return "error";
     }
 }
